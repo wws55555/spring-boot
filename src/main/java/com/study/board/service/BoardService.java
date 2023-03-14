388fda3ab8f0ffcaf47990472e296eaffd28971b
wws55555 <wws555555@naver.com>
2023-03-15 07:21:33 +0900

    first commit

---
+++ src/main/java/com/study/board/service/BoardService.java
@@ -0,0 +1,32 @@
+package com.study.board.service;
+
+import com.study.board.entity.Board;
+import com.study.board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardService {
+    @Autowired
+    private BoardRepository boardRepository;
+    public void write(Board board) {
+        boardRepository.save(board);
+   }
+
+   public Page<Board> boardList(Pageable pageable) {
+        return boardRepository.findAll(pageable);
+   }
+   public Page<Board> boardSearchList(String searchKeyword, Pageable pageable) {
+       return boardRepository.findByTitleContaining(searchKeyword, pageable);
+   }
+   public Board boardView(Integer id) {
+       return boardRepository.findById(id).get();
+   }
+   public void boardDelete(Integer id) {
+        boardRepository.deleteById(id);
+   }
+}
